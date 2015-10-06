53559a977a9b0bcce9c568728c060d6b64d6aa31
mgoldchild <mgoldchild@gmail.com>
2015-10-07 08:08:22 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+#NMV
+Visualize the ranking of the module in your repository.
+
+##install
+```
+npm install -g nmv
+```
+
+##usage
+parameters
+
+##example
+```
+cd /your/repository/path
+```
+
+##hacks
+
+
+##references
+esprima.org/demo/parse.html
+https://estools.github.io/esquery/
---
+++ bin/app.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var async = require('async');
+var program = require('commander');
+var pkg = require(path.join(__dirname, '..', 'package.json'));
+var readfile = require(path.join(__dirname, '..', 'lib', 'file'));
+var readAst = require(path.join(__dirname, '..', 'lib', 'read'));
+var printResult = require(path.join(__dirname, '..', 'lib', 'print'));
+
+program
+  .version(pkg.version)
+  .option('-s, --show', 'show your commit')
+  .arguments('<cmd> [env]')
+  .parse(process.argv);
+
+var src = (program.args.length === 0) ? '.' : program.args[0];
+
+async.waterfall([
+  function(callback) {
+    readfile(src, function(err, files) {
+      if (err) callback(err);
+      callback(null, files);
+    });
+  },
+  function(files, callback) {
+    readAst(files, function(err, requires) {
+      if (err) callback(err);
+      callback(null, requires);
+    });
+  }
+],function(err, result) {
+  if (err) throw new Error('Require Module Faild', err);
+  printResult(result);
+});
+
